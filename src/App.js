d2abddaba1b22aa8437aebd9b2a6774987fc30bb
coderDaiyan <daiyanabdallah@gmail.com>
2022-10-21 23:38:49 +0600

    init: initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import addNotification from "react-push-notification";
+import { io } from "socket.io-client";
+import "./App.css";
+
+const socket = io("http://localhost:8080");
 
 function App() {
+  const [msg, setMsg] = useState("");
+  const [chats, setChats] = useState([]);
+
+  const sendChat = (e) => {
+    e.preventDefault();
+    socket.emit("chat", { msg, user: socket.id });
+    addNotification({
+      title: "Success!",
+      subtitle: "This is a subtitle",
+      message: "Your message sent successfully :))",
+      theme: "darkblue",
+      native: true, // when using native, your OS will handle theming.
+    });
+    setMsg("");
+  };
+
+  useEffect(() => {
+    socket.on("chat", (payload) => setChats([...chats, payload]));
+  }, [chats]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Chatty</h1>
+        <div>
+          {chats.map(({ msg: message, user }, index) => {
+            return (
+              <>
+                <div
+                  className={`${
+                    socket.id === user ? "msg_body" : "other_msg_body"
+                  }`}
+                >
+                  <p key={index}>{message}</p>
+                  <span>ID: {user.substr(0, 5)}</span>
+                </div>
+              </>
+            );
+          })}
+        </div>
+        <form onSubmit={sendChat}>
+          <input
+            type="text"
+            name="chat"
+            placeholder="Write your message"
+            value={msg}
+            onChange={(e) => setMsg(e.target.value)}
+          />
+          <button type="submit">Send</button>
+        </form>
       </header>
     </div>
   );
